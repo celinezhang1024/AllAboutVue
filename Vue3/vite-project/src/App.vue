4d2aa7fb3a38eb749fedf7a9e79dcc9bcbe58f2d
wanjingzhang <95616085@qq.com>
2022-07-21 05:41:24 +0800

    基础模版语法

--- Vue3/vite-project/src/App.vue
+++ Vue3/vite-project/src/App.vue
@@ -1,31 +1,17 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+  import * as HelloWorldVue from './components/HelloWorld.vue';
+  const message:string = 'Celine'
+  const flag:boolean = false 
+  const number:number = 0;
+  const string:string = 'C,e,l,i,n,e'
 </script>
 
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<template> 
+  <div>{{message}} - 模版语法 </div>
+  <div>{{flag?"真的":"假的"}} - 条件</div> 
+  <div>{{number+1}} - 运算</div>
+  <div>{{string.split(',').map(v => `<div>${v}</div>`)}}</div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style scoped> 
 </style>
