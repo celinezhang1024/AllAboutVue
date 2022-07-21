60b3165a4b93c6d5ba8ede950af1e702c30c2228
wanjingzhang <95616085@qq.com>
2022-07-22 07:58:18 +0800

    ref

--- Vue3/vite-project/src/components/Ref.vue
+++ Vue3/vite-project/src/components/Ref.vue
@@ -3,7 +3,7 @@
 </style>
 
 <template>
-    <div>1. v-model+钩子函数 <input v-model="userName" type="text" /> {{userName}} </div> 
+    <div>1. v-model+钩子函数 <input v-model="userName1" type="text" /> {{userName1}} </div> 
     <div>2. Ref 钩子函数-接口 {{userName2}} <button @click="changeMsg">改变hook的值</button></div> 
     <div>3. 浅复制 shallowRef {{shallowMsg}} <button @click="changeShllowMsg">修改shallow的value</button></div>
     <div>4. triggerRef 强制更新视图 {{shallowMsg}}</div>
@@ -12,7 +12,7 @@
 
 <script setup lang="ts"> 
   import { ref, Ref, isRef, shallowRef, triggerRef, customRef } from 'vue' 
-  let userName = ref<string | number>("test"); // 响应式 钩子函数, 联合类型
+  let userName1 = ref<string | number>("test"); // 响应式 钩子函数, 联合类型
   let userName2:Ref<string> = ref("Celine"); //  Ref接口
   let notRef:number = 1; 
 
@@ -22,15 +22,15 @@
     console.log("是不是Ref",isRef(notRef))  
   }
 
-  // 只跟踪自身的变换.value，不改变视图
+  // 3. 只跟踪自身的变换.value，不改变视图
   type Obj = {name:string}
   let shallowMsg:Ref<Obj> = shallowRef({name:"Celine"})
   const changeShllowMsg = () =>{
-    // 1. 无法修改 用属性，可配合3
+    // 3. 无法修改 用属性，可配合3
     shallowMsg.value.name = "Celine abc"
-    // 2. 可以修改 用对象
+    // 3. 用对象 可以修改 
     // shallowMsg.value = {name:"Celine abc"}
-    // 3. 强制更新视图
+    // 4. 强制更新视图
     triggerRef(shallowMsg)
     console.log(shallowMsg.value.name)
   }
