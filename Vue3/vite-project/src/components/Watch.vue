f2f9f8fe830f97e57bb264d831e3acac67db3ffb
Celine <95616085@qq.com>
2022-07-22 15:55:53 +0800

    watch 方法

--- Vue3/vite-project/src/components/Watch.vue
+++ Vue3/vite-project/src/components/Watch.vue
@@ -1,16 +1,23 @@
 <template>
+  同事监听2个值
   <input v-model="message" type="text" />
+  <br/>
   <input v-model="message2" type="text" />
-
+  <br/>
+  ref deep:true
   <input v-model="obj.nav.bar.name" type="text"/>
+  <br/>
+  reactive
+  <input v-model="rectiveObj.nav.bar.name" type="text"/>
+  <br/>
 </template>
 
 <script setup lang="ts">
-  import {ref,watch} from 'vue'
+  import {ref,watch,reactive} from 'vue'
   // 1. 多个数据的绑定
   let message = ref<string>('')
   let message2 = ref<string>('') 
-  
+
   watch([message,message2],(newVal,oldVal) =>{
     console.log("新的",newVal)
     console.log("旧的",oldVal)
@@ -27,7 +34,23 @@
 
   watch(obj,(newVal,oldVal) =>{
     console.log('新的：' , newVal) 
+    console.log("旧的", oldVal)
   },{
-    deep:true
+    deep:true,
+    immediate:true // 默认第一次跳过watch，所以第一次oldVal是undefined
+  }) 
+
+  // 3. reactive
+  let rectiveObj = reactive({
+    nav:{
+      bar:{
+        name:"Celine"
+      }
+    }
   })
+  watch(rectiveObj,(newVal,oldVal)=>{
+    console.log('新的：' , newVal) 
+    console.log("旧的", oldVal)
+  })
+
 </script>
