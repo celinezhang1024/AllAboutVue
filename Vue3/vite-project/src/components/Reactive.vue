caa548846e4dcebf5bb9fcd027d6326c045615ec
wanjingzhang <95616085@qq.com>
2022-07-22 08:29:48 +0800

    reactive

--- Vue3/vite-project/src/components/Reactive.vue
+++ Vue3/vite-project/src/components/Reactive.vue
@@ -1,12 +1,27 @@
 <!-- 响应式副本 -->
 <template>
-    <div></div>
+    <div>
+        <div>arr {{ message }}</div>
+        <div>object {{ obj }}</div>
+    </div>
 </template>
 
 <script setup lang="ts">
 import { reactive } from 'vue'
-let message = reactive([])
+let message:Array<number> = reactive([])
 
+let obj = reactive({
+    name: "Celine"
+})
+obj.name = "Celine 2022" // 动态改变数据
+
+// 直接赋值数据，会破坏响应式数据
+setTimeout(()=>{
+    let arr:Array<number> = [1,2,3,4]
+    // message = arr  //🔥
+    message.push(...arr)
+    console.log(message)
+})
 
 </script>
 
