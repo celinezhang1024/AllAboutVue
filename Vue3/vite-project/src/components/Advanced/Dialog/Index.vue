c75ccdd125b5a810f5d2e7b74ff0e6b7f0aabaae
wanjingzhang <95616085@qq.com>
2022-07-23 14:48:52 +0800

    具名插槽,匿名插槽,插槽作用域

---
+++ Vue3/vite-project/src/components/Advanced/Dialog/Index.vue
@@ -0,0 +1,36 @@
+<!-- 插槽 -->
+<template>
+   <div>
+       <header class="header"> <!--具名插槽-->
+        <slot name="header"></slot> 
+       </header>
+       <main class="main">
+        <div v-for="(item,index) in data"> 
+            <slot :data="item" :index="index"></slot> <!--匿名插槽 父组件传值到子组件 -->
+        </div>
+       </main>
+       <footer class="footer">
+        <slot name="footer"></slot>
+       </footer>
+   </div>
+</template>
+
+<script setup lang='ts'>
+import {reactive} from 'vue'
+type person = {name:string,age:number}
+const data = reactive<person[]>([{
+    name:"celine",age:20
+},
+{
+    name:"nancy",age:30
+},{
+    name:"manda",age:40
+}])
+
+</script>
+
+<style scoped lang='less'>
+.header{height: 200px;background: red;}
+.main{height: 200px;background: green;}
+.footer{height: 200px;background: blue;}
+</style>
--- Vue3/vite-project/src/components/Advanced/Layout/Content/Index.vue
+++ Vue3/vite-project/src/components/Advanced/Layout/Content/Index.vue
@@ -7,13 +7,28 @@
         </div> -->
         <!-- 2. Is 动态组件 -->
         <!-- <Is /> -->
-        <Vue2 />
+        <!-- <Vue2 /> -->
+        <!-- 3. 插槽 -->
+        <Dialog>
+            <template #header> <!-- v-slot: = # -->
+                <div>插上面</div>
+            </template>
+            <template v-slot="{data,index}">
+                <div>{{data.name}} - {{data.age}} - {{index}}</div><!--插槽作用域-->
+            </template>
+            <template v-slot:footer>
+                <div>插下面</div>
+            </template>
+        </Dialog>
     </div>
 </template>
 
 <script setup lang="ts">
 import Is from "../../Is/index.vue"
 import Vue2 from "../../Is/Vue2.vue"
+
+// 插槽
+import Dialog from "../../Dialog/Index.vue"
 
 </script>
 
