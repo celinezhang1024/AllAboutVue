915ea8538272b2c3f6c28d50c255001989a5b636
wanjingzhang <95616085@qq.com>
2022-07-24 17:35:19 +0800

    2. 状态过渡

--- Vue3/vite-project/src/components/Advanced/DynamicCard/Index.vue
+++ Vue3/vite-project/src/components/Advanced/DynamicCard/Index.vue
@@ -1,16 +1,27 @@
-<!-- 动态洗牌的效果 -->
+<!-- 动态洗牌的效果  -->
 <template>
    <div>
+        <!-- 1. 列表移动过度 -->
+        <div>1. 列表移动过度 </div>
         <button @click="random">random</button>
-       <transition-group move-class="moveAni" class="wraps" tag="div">
+        <transition-group move-class="moveAni" class="wraps" tag="div">
            <div class="items" :key="item.id" v-for="item in list">{{item.number}}</div>
-       </transition-group>
+        </transition-group>
+        <!-- 2. 状态过渡 -->
+        <div>2. 状态过渡 </div>
+        <div>
+            <input v-model="num.current" step="20" type="text">
+            <div>
+                {{num.tweenedNumber.toFixed(2)}}
+            </div>
+        </div>
    </div>
 </template>
 
 <script setup lang='ts'>
-import {ref} from 'vue'
+import {ref,reactive,watch} from 'vue'
 import _ from 'lodash'
+import gsap from 'gsap'
 
 // 随机使用lodash._shuffle()方法  
 // npm install lodash -S 
@@ -24,6 +35,18 @@ let list = ref(Array.apply(null,{length:81} as number[]).map((_,index)=>{
 const random = ()=>{
     list.value = _.shuffle(list.value)
 }
+
+// 2.
+const num = reactive({
+    current:0,
+    tweenedNumber:0
+})
+watch(()=>num.current,(newVal,oldVal)=>{
+    gsap.to(num,{
+        duration:1,
+        tweenedNumber:newVal
+    })
+})
 
 </script>
 
