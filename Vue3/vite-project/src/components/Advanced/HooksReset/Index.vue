9ed78ca448749cf2228add3444d356c6f4dcd848
Celine <95616085@qq.com>
2022-07-25 16:58:52 +0800

    vueuse plugin

--- Vue3/vite-project/src/components/Advanced/HooksReset/Index.vue
+++ Vue3/vite-project/src/components/Advanced/HooksReset/Index.vue
@@ -2,12 +2,24 @@
   <div >
     <img id="img" width="300" height="300" src="../../../assets/vue.svg"/>
     <!-- <A a="456" title="123"></A> -->
+    {{x}} {{y}}
   </div>
 </template>
  
 <script setup lang='ts'>
 // import A from "./A.vue"
 import useBase64 from './hooks'
+import { useMouse } from '@vueuse/core'
+
+// "x" and "y" are refs
+const { x, y } = useMouse()
+
+console.log(x.value)
+
+const mouse = useMouse()
+
+console.log(mouse.x.value)
+
 
 useBase64({ el:'#img'}).then(res=>{
   console.log(res.baseUrl); 
