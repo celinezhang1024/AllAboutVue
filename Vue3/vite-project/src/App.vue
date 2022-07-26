3a86d6c328118f7bc5be91cf4c4dc4e1ae2ee222
Celine <95616085@qq.com>
2022-07-26 16:07:21 +0800

    NextTick

--- Vue3/vite-project/src/App.vue
+++ Vue3/vite-project/src/App.vue
@@ -35,7 +35,8 @@
     <!-- <Directive2/> -->
     <!-- <Directive3/> -->
     <!-- <HooksReset/> -->
-    <Plugin/>
+    <!-- <Plugin/> -->
+    <NextTick/>
   </div>
 </template>
 
@@ -65,6 +66,7 @@
   import Directive3 from './components/Advanced/Directive/Index3.vue'
   import HooksReset from './components/Advanced/HooksReset/Index.vue'
   import Plugin from './components/Advanced/Loading/Main.vue'
+  import NextTick from './components/Advanced/NextTick/Index.vue'
    
   const API = defineAsyncComponent(()=> import("./components/Advanced/API/Index.vue"))
 
---
+++ Vue3/vite-project/src/components/Advanced/NextTick/Index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div >
+    <input v-model="message" type="text">
+    <div ref="div">{{message}}</div>
+    <button @click="change">change</button>
+  </div>
+</template>
+ 
+<script setup lang='ts'>
+import {ref,nextTick} from 'vue'
+const message = ref<string>('Celine')
+const div = ref<HTMLElement>()
+const change = async () =>{
+  message.value = 'Celine best!' 
+  await nextTick()
+  console.log(div.value?.innerText) // 监听不到最新的值
+
+}
+</script>
+ 
+<style scoped lang="less">
+ 
+</style> 
