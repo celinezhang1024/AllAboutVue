5cc26890507d7b24c8eae0ff30a88530aea86317
wanjingzhang <95616085@qq.com>
2022-07-27 08:16:41 +0800

    Pinia getters actions

--- Vue3/vite-project/src/components/Advanced/Pinia/Index.vue
+++ Vue3/vite-project/src/components/Advanced/Pinia/Index.vue
@@ -1,14 +1,19 @@
 <!-- Pina数据管理 代替vuex  -->
 <template>
    <div>
-       pinia:{{Test.current}} - {{Test.name}}
-       <button @click="change">change</button>
+       pinia:{{Test.current}} - {{Test.name}} <br/>
+       解构的值:{{current}} - {{name}} 不是响应式的 <br/> 
+       <button @click="change">change</button> <br/> 
+       storeToRefs{{ cur }} {{ nam}} <br/> 
+       <p>action-user:  {{Test.user}}</p>
+       <p>action-name:  {{Test.name}}</p>
+       <p>getter: {{Test.newName}}</p>
    </div>
 </template>
 
 <script setup lang='ts'>
 import {useTestStore} from '../store'
-
+import { storeToRefs } from 'pinia';
 const Test = useTestStore()
 // 1. 直接修改 Test.current++
 // 2. 通过 $patch 修改整个对象
@@ -28,10 +33,16 @@ const Test = useTestStore()
 //     current:22,
 //     name:'abc'
 // }
-// 5. action修改
+// 5. action修改 Test.setCurrent(0.2)
+
+// Store
+const {current,name} = Test
+const {current:cur,name:nam} = storeToRefs(Test)
 
 const change = ()=>{
-    Test.setCurrent(0.2)
+    // Test.current++
+    // ref包裹的值 cur.value ++
+    Test.setUser()
 }
 
 </script>
--- Vue3/vite-project/src/components/Advanced/store/index.ts
+++ Vue3/vite-project/src/components/Advanced/store/index.ts
@@ -1,21 +1,56 @@
 import {defineStore} from 'pinia'
 import {Names} from './store-name'
 
+type User = {
+    name:string,
+    age:number
+}
+
+let result:User = {
+    name:'Celine',
+    age:18
+}
+
+const Login = (): Promise<User> =>{
+    return new Promise((resolve)=>{
+        setTimeout(() => {
+            resolve({
+                name:'Celine 女王',
+                age: 17
+            })
+        }, 2000);
+    })
+}
+
 export const useTestStore = defineStore(Names.TEST,{
     state:()=> {
         return {
+            user:<User>{age:1},
             current: 1000,
             name: 'Celine女王'
         }
     },
     // computed 修饰一些值
     getters:{
-
+        newName():string{ // 方法，使ts无法推断返回值类型
+            return `$-${this.name}-${this.getUserAge}`
+        },
+        getUserAge():number{
+            return this.user.age
+        }
     },
     // methods 可以做同步 异步，提交state
     actions:{
         setCurrent(num:number){
             this.current = num
+        },
+        async setUser(){
+            const result = await Login() // 异步
+            this.user = result;
+            this.setName('Celine女王 加冕')
+        },
+        setName(name:string){
+            this.name = name;
         }
     }
 })
