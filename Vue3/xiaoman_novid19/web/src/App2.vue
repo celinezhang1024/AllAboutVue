2530f9bab8efe1aa05fde212b74ae4af8bf45a5e
wanjingzhang <95616085@qq.com>
2022-07-29 08:34:54 +0800

    简单路由

--- Vue3/xiaoman_novid19/web/src/App2.vue
+++ Vue3/xiaoman_novid19/web/src/App2.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <h1>Celine</h1>
+    <div> 
+      <router-link to="/" > Login </router-link>
+      <router-link style="marin-left:10px" to="/reg">Reg</router-link>
+    </div>
     <hr>
+    <!-- router容器 -->
     <router-view></router-view>
   </div>
 </template>
